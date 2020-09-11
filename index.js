2ec7d8e44732bb73e04007d3caa5eae3b8ea12a7
Ju minhark <wnalsgkr@gmail.com>
2020-09-11 09:56:07 +0900

    queue&stack

--- index.js
+++ index.js
@@ -1,99 +1,105 @@
+// https://github.com/trekhleb/javascript-algorithms
+
 //? 문제 : 후보키
 
-const candidateKey = (arr) => {
-	const index = [0, 1, 2, 3];
+// const candidateKey = (arr) => {
+// 	let count = 0;
+// 	let index = [0, 1, 2, 3];
 
-	const combinations = (arr, k) => {
-		let combs = [];
-		let head = 0;
-		let tailCombs = 0;
+// 	const combinations = (arr, k) => {
+// 		let combs = [];
+// 		let head = 0;
+// 		let tailCombs = 0;
 
-		if (k > arr.length || k <= 0) {
-			return [];
-		}
+// 		if (k > arr.length || k <= 0) {
+// 			return [];
+// 		}
 
-		if (k == arr.length) {
-			return [arr];
-		}
+// 		if (k == arr.length) {
+// 			return [arr];
+// 		}
 
-		if (k == 1) {
-			combs = [];
-			for (i = 0; i < arr.length; i++) {
-				combs.push([arr[i]]);
-			}
-			return combs;
-		}
+// 		if (k == 1) {
+// 			combs = [];
+// 			for (i = 0; i < arr.length; i++) {
+// 				combs.push([arr[i]]);
+// 			}
+// 			return combs;
+// 		}
 
-		for (let i = 0; i < arr.length - k + 1; i++) {
-			head = arr.slice(i, i + 1); // start , end (end미포함)
-			tailCombs = combinations(arr.slice(i + 1), k - 1);
-			for (let j = 0; j < tailCombs.length; j++) {
-				combs.push(head.concat(tailCombs[j]));
-			}
-		}
+// 		for (let i = 0; i < arr.length - k + 1; i++) {
+// 			head = arr.slice(i, i + 1); // start , end (end미포함)
+// 			tailCombs = combinations(arr.slice(i + 1), k - 1);
+// 			for (let j = 0; j < tailCombs.length; j++) {
+// 				combs.push(head.concat(tailCombs[j]));
+// 			}
+// 		}
 
-		return combs;
-	};
+// 		return combs;
+// 	};
 
-	console.log(combinations(index, 2));
-	// unit 인것을 찾으면 index에서 제거.
+// 	console.log(combinations(index, 2));
+// 	// unit 인것을 찾으면 index에서 제거.
 
-	let compare = [];
+// 	const arrayCompareOneUnit = (arr) => {
+// 		for (let i = 0; i < arr.length; i++) {
+// 			let t = arr.slice();
+// 			let a = arr[i][0]; // 비교대상
+// 			console.log('a : ' + a);
+// 			t.splice(i, 1); // 비교대상을 제외한 나머지
+// 			console.log(t);
 
-	// for (let unit of combinations(index, 1)) {
-	// 	for (let i = 0; i < arr.length; i++) {
-	// 		compare.push(arr[i][unit]);
-	// 	}
-	// 	console.log(compare);
-	// 	compare = [];
-	// }
-	for (let j = 1; j < 4; j++) {
-		let resultMap = combinations(index, j);
+// 			if (
+// 				t.every((item) => {
+// 					console.log('item[0] : ' + item[0]);
+// 					return item[0] !== a;
+// 				})
+// 			) {
+// 				// 배열안에 모든원소가 다를때
+// 				console.log('다르다');
+// 			} else {
+// 				console.log('같은것이 있다.');
+// 				break;
+// 			}
+// 		}
+// 	};
 
-		for (let unit of resultMap) {
-			console.log(unit);
-			for (let i = 0; i < arr.length; i++) {
-				let temp = [];
-				// console.log(i);
-				for (let u of unit) {
-					// console.log(u);
-					temp.push(arr[i][u]);
-				}
-				compare.push(temp);
-			}
-			console.log(compare);
-			// compare에 중복값이 있는지 확인
-			console.log(compare[0]);
-			console.log(compare[1]);
-			compare = [];
-		}
-	}
+// 	let compare = [];
 
-	// 배열안에 모든 원소가 다른지 확인
-	// 	const check = (ar) => {
-	// 		ar.length =>
-	// a of ar
-	// ar: +1
-	// ar == ar.length
+// 	for (let j = 1; j < 4; j++) {
+// 		let resultMap = combinations(index, j);
 
-	// 		return 1
-	// 	}
-	// };
+// 		for (let unit of resultMap) {
+// 			console.log(unit);
+// 			for (let i = 0; i < arr.length; i++) {
+// 				let temp = [];
+// 				// console.log(i);
+// 				for (let u of unit) {
+// 					// console.log(u);
+// 					temp.push(arr[i][u]);
+// 				}
+// 				compare.push(temp);
+// 			}
+// 			console.log(compare);
+// 			// 2중 배열안에 배열의 중복 여부
+// 			console.log(arrayCompareOneUnit(compare));
 
-	// 자신 자신을 포함하고 있는 또다른 배열을 지운다
-	// f(3) => f(2) + 1 => 점화식 ㅇ
-	// 재귀  => 재귀
+// 			compare = [];
+// 		}
+// 	}
 
-	// return arr.length;
-};
+// 	// return count;
+// };
 
-console.log(
-	candidateKey([
-		['100', 'ryan', 'music', '2'],
-		['200', 'apeach', 'math', '2'],
-		['300', 'tube', 'computer', '3'],
-		['400', 'con', 'computer', '4'],
-		['500', 'muzi', 'music', '3'],
-		['600', 'apeach', 'music', '2'],
-	])
-);
+// console.log(
+// 	candidateKey([
+// 		['100', 'ryan', 'music', '2'],
+// 		['200', 'apeach', 'math', '2'],
+// 		['300', 'tube', 'computer', '3'],
+// 		['400', 'con', 'computer', '4'],
+// 		['500', 'muzi', 'music', '3'],
+// 		['600', 'apeach', 'music', '2'],
+// 	])
+// );
+
+//======================================================
--- theory/basic/stack_queue.js
+++ theory/basic/stack_queue.js
@@ -31,6 +31,7 @@ queue.shift();
 // console.log(queue.reverse());
 
 // arr.shift() : 배열에서 첫 번째 요소를 삭제하고 그 요소를 반환.
+// arr.unshift() : 배열에서 첫 번째 요소를 삭제하고 그 요소를 반환.
 
 //? 문제: 기능개발
 // 각 기능은 진도가 100%일 때 서비스에 반영
@@ -78,4 +79,250 @@ const developmentFunction = (progresses, speeds) => {
 // console.log(developmentFunction([93, 30, 55], [1, 30, 5]));
 console.log(developmentFunction([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]));
 
+//* queue stack
+
 //? 문제 : 다리를 지나는 트럭
+
+// bridge_length : 다리 길이, weight: 다리가 견디는 무게, truck_weights: 대기 트럭
+// 1초에 1씩 트럭 이동.
+
+const bridge = (bridge_length, weight, truck_weights) => {
+	let br = Array(bridge_length).fill(0);
+	let countWeight = 0;
+	let sec = 0;
+	let last = truck_weights[truck_weights.length - 1];
+
+	// console.log(br);
+	// console.log(last);
+	let start = true;
+	while (start) {
+		let check = truck_weights[0]; // 다리로 진입할 트럭의 무게
+		countWeight -= br[0]; // 다리위를 통과할 트럭을 전체 무게에서 빼준다
+		// console.log('check : ' + check);
+		// console.log('countWeight : ' + countWeight);
+
+		// 다리 진행중 상황
+		if (countWeight + check <= weight) {
+			let unit = truck_weights.shift();
+			br.push(unit);
+			countWeight += unit;
+		} else {
+			br.push(0);
+		}
+
+		sec += 1;
+		br.shift();
+		// console.log(br);
+		// console.log(sec);
+
+		let endCheck = br.every((item) => {
+			return item == 0;
+		});
+
+		// 종료시점
+		if (truck_weights.length == 0 && endCheck) {
+			start = false;
+		}
+	}
+	return sec;
+};
+
+// console.log(bridge(2, 10, [7, 4, 5, 6])); //8
+// console.log(bridge(100, 100, [10])); // 101
+// console.log(bridge(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10])); //110
+
+//? 문제 : 프린터
+
+// 프린터는 배열의 앞에서 부터 인쇄
+// 프린터 될 순서의 문서가 대기 문서의 중요도 보다 낮으면 대기열 맨 뒤로 이동
+
+//! 문제 잘못이해
+
+const print = (priorities, location) => {
+	let result = String(Number(location) + 1);
+
+	console.log(result);
+	// 배열 재정의
+	let importantMap = {};
+	for (let unit in priorities) {
+		importantMap[Number(unit) + 1] = priorities[unit];
+	}
+	console.log(importantMap);
+
+	for (let unit in priorities) {
+		priorities[unit] = String(Number(unit) + 1);
+	}
+	console.log(priorities);
+
+	console.log(importantMap[priorities[2]]);
+
+	let start = true;
+	while (start) {
+		let check = importantMap[priorities[0]];
+		let arr = priorities.slice(1);
+
+		let end = arr.every((i) => {
+			return check >= importantMap[i];
+		});
+
+		if (end) {
+			start = false;
+		} else {
+			let unit = priorities.shift();
+			priorities.push(unit);
+		}
+	}
+
+	console.log(importantMap);
+	console.log(priorities);
+
+	return priorities.indexOf(result) + 1;
+};
+
+// console.log(print([2, 1, 3, 2], 2));
+// console.log(print([1, 1, 9, 1, 1, 1], 0));
+// console.log(print([2, 2, 2, 1, 3, 4], 3));
+
+//? 문제 : 프린터
+
+// 프린터는 배열의 앞에서 부터 인쇄
+// 프린터 될 순서의 문서가 대기 문서의 중요도 보다 낮으면 대기열 맨 뒤로 이동
+
+//! 문제 잘못이해
+
+const print2 = (priorities, location) => {
+	let resultMap = [];
+
+	for (let unit in priorities) {
+		resultMap.push(unit);
+	}
+	// console.log(resultMap);
+
+	let start = true;
+	while (start) {
+		let check = priorities[0];
+		let arr = priorities.slice(1);
+
+		let end = arr.every((i) => {
+			return check >= i;
+		});
+
+		if (end) {
+			start = false;
+		} else {
+			let unit = priorities.shift();
+			priorities.push(unit);
+			let p = resultMap.shift();
+			resultMap.push(p);
+		}
+	}
+
+	// console.log(priorities);
+	// console.log(resultMap);
+
+	return resultMap.indexOf(String(location)) + 1;
+};
+
+// console.log(print2([2, 1, 3, 2], 2));
+// console.log(print2([1, 1, 9, 1, 1, 1], 0));
+// console.log(print2([2, 2, 2, 1, 3, 4], 3));
+
+//? 문제 : 프린터
+
+// 프린터는 배열의 앞에서 부터 인쇄
+// 프린터 될 순서의 문서가 대기 문서의 중요도 보다 낮으면 대기열 맨 뒤로 이동
+
+const print3 = (priorities, location) => {
+	let result = String(Number(location) + 1);
+	// 배열 재정의
+	let importantMap = {};
+	for (let unit in priorities) {
+		importantMap[Number(unit) + 1] = priorities[unit];
+	}
+	// console.log(importantMap);
+
+	for (let unit in priorities) {
+		priorities[unit] = String(Number(unit) + 1);
+	}
+	// console.log(priorities);
+
+	let start = true;
+	let resultMap = [];
+	while (start) {
+		let check = importantMap[priorities[0]];
+		let arr = priorities.slice(1);
+
+		// 종료시점
+		let end = arr.every((i) => {
+			return check >= importantMap[i];
+		});
+
+		if (end) {
+			if (priorities.length == 0) {
+				start = false;
+			} else {
+				let unit = priorities.shift();
+				resultMap.push(unit);
+			}
+		} else {
+			let unit = priorities.shift();
+			priorities.push(unit);
+		}
+	}
+
+	// console.log(importantMap);
+	// console.log(priorities);
+	// console.log(result);
+
+	return resultMap.indexOf(result) + 1;
+};
+
+// console.log(print3([2, 1, 3, 2], 2));
+// console.log(print3([1, 1, 9, 1, 1, 1], 0));
+// console.log(print3([2, 2, 2, 1, 3, 4], 3));
+
+//? 문제 : 주식 가격
+
+// 시간별 주식 가격 prices [1, 2, 3, 2, 3] 가 주어 질때
+// 가격이 원래 가격보다 떨어지는 시점이 기록된 return
+// 1초 시점의 ₩1은 끝까지 가격이 떨어지지 않았습니다.
+// 2초 시점의 ₩2은 끝까지 가격이 떨어지지 않았습니다.
+// 3초 시점의 ₩3은 1초뒤에 가격이 떨어집니다. 따라서 1초간 가격이 떨어지지 않은 것으로 봅니다.
+// 4초 시점의 ₩2은 1초간 가격이 떨어지지 않았습니다.
+// 5초 시점의 ₩3은 0초간 가격이 떨어지지 않았습니다.
+// [4,3,1,1,0]
+
+const price = (arr) => {
+	let result = [];
+	for (let unit in arr) {
+		let compareList = arr.slice(Number(unit) + 1);
+		let i = 0;
+		// 마지막날
+		if (Number(unit) === arr.length - 1) {
+			result.push(0);
+		} else {
+			while (arr[unit] <= compareList[i]) {
+				i++;
+				console.log(i);
+			}
+			if (i == 0) {
+				result.push(1);
+			} else {
+				result.push(i);
+			}
+		}
+	}
+	return result;
+};
+
+console.log(price([1, 2, 3, 2, 3])); // [4,3,1,1,0]
+
+//! for()문 안에서 break를 만나면 for문을 종료, label에 관한내용.
+// loof1 : for( )
+// loof 2 : for (let i = 0; i < arr.length; i++) {
+// 	if (i == 2) {
+// 		break : loof1;
+// 	}
+// 	console.log(i);
+// }
+//}
