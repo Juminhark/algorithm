cb54aa38f4623f29618e4fe144148789713dc5ca
Juminhark <40381241+Juminhark@users.noreply.github.com>
2022-09-23 17:37:29 +0900

    summary

---
+++ index.js
@@ -0,0 +1,9 @@
+//? String.prototype.substring()
+
+//? sting객체의 시작인덱스부터 [종료인덱스] 전 까지 부분 문자열 반환
+
+const str = 'Mozilla';
+
+console.log(str.substring(1, 3)); // oz
+
+console.log(str.substring(2)); // zilla
