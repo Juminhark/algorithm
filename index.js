5e42fec17bad566f02ff5e762026ea76cf05b990
Ju minhark <wnalsgkr@gmail.com>
2022-10-12 13:37:42 +0900

    sumarry

--- index.js
+++ index.js
@@ -1,43 +1,34 @@
-const solution = (polynomial) => {
-    let answer = '';
-    let count = [0,0];
-    let str = polynomial.split(" + ")
+const solution = (X, Y) => {
+    let answer = [];
+    let arr_x = X.split('').sort()
+    let arr_y = Y.split('').sort()
+    
+    let count_x = 0;
+    let count_y = 0;
 
-    str.forEach((e) => {
-        if(e.includes('x')) {
-
-            if(e === 'x'){
-                count[0] += 1
-            } else if(e === '-x') {
-                count[0] += -1
-            } else {
-                let tmp = e.replace('x','')
-                count[0] += Number(tmp)
-            }       
-
+    while(count_x < arr_x.length && count_y < arr_y.length){
+        if(arr_x[count_x] === arr_y[count_y]) {
+            answer.push(arr_x[count_x])
+            count_x++;
+            count_y++;
         } else {
-            count[1] += Number(e) 
+            if(arr_x[count_x] < arr_y[count_y]) count_x++;
+            else count_y++;
         }
-    })
-
-    console.log(count)
-
-    if(count[0] === 1){
-        answer += 'x'
-    } else if (count[0] > 1) {
-        answer += count[0]
-        answer += 'x'
-    }
-
-    if(count[0] === 0 && count[1] > 0){
-        answer += count[1]
-    } else if(count[1] > 0){
-        answer += ' + '
-        answer += count[1]
     }
     
+    
+    if(answer.length === 0) return '-1'
+
+    answer = answer.sort((a,b) => b-a).join('')
+
+    if(Number(answer) === 0) return '0'
+
     return answer;
 }
 
-console.log(solution("x + 7 + -x"))
-// console.log(solution("x + x + x"))
+console.log(solution("100", "2345"))
+console.log(solution("100", "203045"))
+console.log(solution("100", "123450"))
+console.log(solution("12321", "42531"))
+console.log(solution("5525", "1255"))
