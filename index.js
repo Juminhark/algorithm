d0711f34ca36113e289a3cc7e17e01ed3982db81
Ju minhark <wnalsgkr@gmail.com>
2023-04-03 12:16:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,30 +14,6 @@
 // }
 
 
-// console.log(solution(12))
 
-
-const solution = (id_pw, db) => {
-    let answer = '';
-    let db_obj = {};
-    
-    db.forEach((e) => {
-        let [a,b] = e;
-        db_obj[a] = b;
-    })
-
-    let [id, pw] = id_pw
-
-    const pw_check = (a, b) => {
-        if(a === b) return 'login'
-        else return 'wrong pw'
-    }
-
-    db_obj[id] ? 
-    answer = pw_check(db_obj[id],pw)
-     : answer = 'fail'
-
-    return answer;
-}
 
 console.log(solution(["meosseugi", "1234"], [["rardss", "123"], ["yyoom", "1234"], ["meosseugi", "1234"]]))
