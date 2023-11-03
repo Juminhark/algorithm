7afcda971c9cc72dcd6aee919532a0f014c0b9ca
Juminhark <40381241+Juminhark@users.noreply.github.com>
2023-11-03 18:39:33 +0900

    summary

---
+++ programmers/level_0/0.120837.js
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120837
+//? 문제 : 개미 군단
+
+//? [5,3,1] 공격력을 가진 개미들을 사냥감 체력에 맞게 대리고 나갈 때,
+//? 최소한의 병력을 구성하려면 몇마리인가 return
+
+const solution = (hp) => {
+  let answer = 0;
+
+  answer += Math.floor(hp / 5);
+  hp = hp % 5;
+
+  answer += Math.floor(hp / 3);
+  hp = hp % 3;
+
+  answer += hp;
+
+  return answer;
+};
+
+console.log(solution(999));
